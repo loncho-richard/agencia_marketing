c52c862bdbdcbfabdb8022ac4ecdf267c8ae3b86
Carlos Richardson <carlosalbertorichardson@gmail.com>
2023-07-03 17:05:02 -0300

    commit with some changes

--- src/components/navigation/Navbar.jsx
+++ src/components/navigation/Navbar.jsx
@@ -7,10 +7,24 @@ import DotLoader from 'react-spinners/DotLoader'
 
 function Navbar(){
     
-    const [loading, setloaging]=useState(true)
+    const [loading, setloaging]=useState(true);
     
+    window.onscroll = function() {scrollFunction()};
+
+    function scrollFunction() {
+        if(document.getElementById('navbar')){
+            if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+                document.getElementById('navbar').classList.add('shadow-navbar');
+                document.getElementById('navbar').classList.add('bg-white');
+            }else{
+                document.getElementById('navbar').classList.remove('shadow-navbar');
+                document.getElementById('navbar').classList.remove('bg-white');
+            }
+        }
+    };
+
     return(
-        <nav className='w-full py-4 top-0 fixed'>
+        <nav id="navbar" className='w-full py-4 top-0 transition duration-300 ease-in-out z-40 fixed'>
             <div className="px-4  sm:px-6">
             <div className="-ml-4 -mt-2 flex flex-wrap items-center justify-between sm:flex-nowrap md:px-12 px-2">
                 <Link to="/" className="ml-4 mt-2">
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,17 +9,15 @@ module.exports = {
         "orange-button":'#ff561e' 
       }
     },
+    boxShadow: {
+      navbar: "0px 10px 8px 0px rgba(3, 3, 4, 0.03), 0 1px 2px -1px rgba(3, 3, 4, 0.03)",
+    },
   },
   plugins: [
-    function ({addUtilities}) {
-      const extendUnderline = {
-        '.underline': {
-          'textDecoration': 'underline',
-          'text-decoration-color': '#ff561e',
-        },
-      }
-      addUtilities(extendUnderline)
-    }
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/line-clamp"),
+    require("@tailwindcss/aspect-ratio"),
   ],
 }
 
